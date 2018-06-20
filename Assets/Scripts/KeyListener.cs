d39a5128083d6eceb1abe23cce42bb16ce79ff57
Daniel Probst <daenuprobst@gmail.com>
2018-06-20 20:36:56 +0200

    Added f and b keys to toggle floor and background

---
+++ Assets/Scripts/KeyListener.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyListener : MonoBehaviour {
+    public Material defaultSkybox;
+    public Material alternativeSkybox;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F))
+        {
+            // Toggle floor
+            var floor = GameObject.Find("Plane").GetComponent<Renderer>();
+
+            if (floor.enabled)
+            {
+                floor.enabled = false;
+            }
+            else
+            {
+                floor.enabled = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            // Toggle background
+            if (RenderSettings.skybox == defaultSkybox)
+            {
+                RenderSettings.skybox = alternativeSkybox;
+            }
+            else
+            {
+                RenderSettings.skybox = defaultSkybox;
+            }
+        }
+	}
+}
